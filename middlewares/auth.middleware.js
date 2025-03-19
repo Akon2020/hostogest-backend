01a8337283d149c6c7afb0880950399d965bf1dd
Isaac Akonkwa <akonkwaushindi@gmail.com>
2025-03-19 08:58:13 +0200

    Auth Middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/env";
+
+export const authenticationJWT = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) return res.redirect("/login");
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) return res.redirect("/login");
+    req.user = user;
+    next();
+  });
+};
